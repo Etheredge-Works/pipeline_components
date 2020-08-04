931efb162442be9b91402fe683c66390d91d3362
Benjamin-Etheredge <benjamin.etheredge@gmail.com>
2020-08-04 03:52:48 +0000

    Adding TODO note.

--- components/wget_url/src/get_data.sh
+++ components/wget_url/src/get_data.sh
@@ -8,6 +8,7 @@ echo "Tar Name: " $tar_name
 echo "Making: " $tar_name
 mkdir -p $tar_name
 wget --directory-prefix=$tar_name/ $data_URL 
+# TODO better naming
 
 # According the the man pages, I'm using -O horribly wrong... I guess I'll just use a dir instead
 #wget -O $tar_name $data_URL 
