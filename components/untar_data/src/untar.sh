6fd6af10cf93db7769b5f20b7b68fb2f9da9b210
Benjamin-Etheredge <benjamin.etheredge@gmail.com>
2020-09-08 17:30:35 +0000

    Fixing typo.

--- components/untar_data/src/untar.sh
+++ components/untar_data/src/untar.sh
@@ -9,5 +9,5 @@ echo "Path to Tar File: " $3
 echo "Tar File Name: " $4
 
 mkdir -p $data_dir
-tar tar_args $path_to_tar_file -C $data_dir --strip-components 1
+tar $tar_args $path_to_tar_file -C $data_dir --strip-components 1
 #tar xzf $path_to_tar_file/$tar_file_name -C $data_dir --strip-components 1
