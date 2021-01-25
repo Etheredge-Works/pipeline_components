a159b4ab84a97f7988166c2cb784332b5ea49c6b
ben <benjamin.etheredge@gmail.com>
2021-01-25 14:48:26 -0600

    Init basic tf dataset builder for oxford data.

---
+++ components/oxford_pet_tf_dataset/build_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+image_name=etheredgeb/oxford_pet_tf_dataset
+image_version=0.1
+
+cd $(dirname $0)
+source ../build.sh
+build_push $(dirname $0) $image_name $image_version
---
+++ components/oxford_pet_tf_dataset/src/main.py
@@ -0,0 +1,57 @@
+
+from pathlib import Path
+import tensorflow as tf
+import os
+
+import click
+@click.command()
+@click.option('--data-dir', type=click.Path(exists=True), help='')
+@click.option('--height', type=click.INT, help='')
+@click.option('--width', type=click.INT, help='')
+@click.option('--tf-data-dir', type=click.Path(exists=False), help='')
+@click.option('--tf-label-dir', type=click.Path(exists=False), help='')
+def main(
+        data_dir: Path, 
+        height: int,
+        width: int,
+        tf_data_dir: Path,
+        tf_label_dir: Path,
+):
+    tf_data_dir.mkdir(parents=True)
+    tf_label_dir.mkdir(parents=True)
+    filename_ds = tf.data.Dataset.list_files(
+        str(data_dir/r'*.jpg'), shuffle=False)
+
+    def file_decode(file_path):
+        img = tf.io.read_file(file_path)
+        img = tf.image.decode_jpeg(img, channels=3)
+
+        # have to resize due to loading ds requiring same size on all
+        img = tf.image.resize(img, [height, width])
+        return img
+
+
+    ds = filename_ds.map(file_decode)
+    tf.data.experimental.save(ds, tf_data_dir)
+
+
+    all_files_tf = tf.io.gfile.listdir(str(data_dir)) # gets just file name
+    labels = tf.strings.regex_replace(all_files_tf, pattern=r'_\d.*', rewrite='')
+
+    def get_label(file_path):
+        # convert the path to a list of path components
+        split = tf.strings.split(file_path, os.path.sep)
+        tf.debugging.assert_greater(tf.size(split), tf.constant(1), f"Split is wrong.\n")
+        file_name = tf.gather(split, tf.size(split) - 1)
+        
+        label = tf.strings.regex_replace(file_name, r'_\d+\.jpg.*', '')
+
+        one_hot = label == labels
+        return tf.argmax(tf.cast(one_hot, tf.uint8)) # must cast since is bool
+
+    label_ds = filename_ds.map(get_label)
+    tf.data.experimental.save(label_ds, tf_label_dir)
+
+
+if __name__ == "__main__":
+    main()
