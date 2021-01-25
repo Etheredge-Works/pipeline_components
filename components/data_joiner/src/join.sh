25ff1342443276f6d5af67f98ee1f0ab970e5493
ben <benjamin.etheredge@gmail.com>
2021-01-24 22:13:02 -0600

    fixing typo

--- components/data_joiner/src/join.sh
+++ components/data_joiner/src/join.sh
@@ -4,6 +4,6 @@ dir2=$2
 out_dir=$3
 
 mkdir -p $out_dir
-copy $dir1/* $out_dir/.
-copy $dir2/* $out_dir/.
+cp $dir1/* $out_dir/.
+cp $dir2/* $out_dir/.
 #tar xzf $path_to_tar_file/$tar_file_name -C $data_dir --strip-components 1
