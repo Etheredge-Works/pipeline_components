42c6b15ed35b80bd9249be012b7228840b65aa81
ben <benjamin.etheredge@gmail.com>
2021-02-07 13:02:55 -0600

    Fixing names

--- components/wget_url/src/get_data.sh
+++ components/wget_url/src/get_data.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 data_URL=$1
-tar_name=$2
+out_dir=$2
 echo "Data URL: " $data_URL
-echo "Tar Name: " $tar_name
+echo "Out Dir: " $out_dir
 #mkdir -p ${tar_name##*/}
 #echo "Making: " ${tar_name##/*}
-echo "Making: " $tar_name
-mkdir -p $tar_name
-wget --directory-prefix=$tar_name/ $data_URL 
+echo "Making: " $out_dir
+mkdir -p $out_dir
+wget --directory-prefix=$out_dir/ "$data_URL"
 # TODO better naming
 
 # According the the man pages, I'm using -O horribly wrong... I guess I'll just use a dir instead
